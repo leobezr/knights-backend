da9b3d65065e28e83d5bf77060da3f4e586a0755
leobezr <le.bezr@gmail.com>
2021-01-13 06:38:04 -0300

    refactor: changed structure of endpoints

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 
-import { getUsers, addUser } from "./repositories/UserRepositories";
+import Routes from "./route.js";
 
 const app = express();
 
@@ -16,24 +16,5 @@ app.use(cors({
 
 app.listen(3333);
 
-// routes
-app.get("/api/v1/knights", async (req, res) => {
-   const users = await getUsers();
-
-   res.json({ users });
-})
-
-app.post("/api/v1/knight/create", async (req, res, next) => {
-   try {
-      const newUser = await addUser(req);
-
-      if (!newUser) {
-         res.status(404).json({ user: {} })
-         throw new Error("404");
-      }
-
-      res.status(201).json({ user: newUser });
-   } catch (e) {
-      next(e)
-   }
-})
+// Sets routes
+Routes(app);
