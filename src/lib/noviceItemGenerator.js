94b187ad7d3f004d6609117a7349190a92ecdce0
leobezr <leonardo.ribeiro@finxi.com.br>
2021-01-17 11:38:11 -0300

    feat: fetches novice set

---
+++ src/lib/noviceItemGenerator.js
@@ -0,0 +1,15 @@
+import deserializeItems from "./deserializeItems";
+
+export default async function () {
+    let itemList = await deserializeItems();
+
+    let armor = itemList.armor.filter(armor => armor.tier == 1)[0]
+    let weapon = itemList.weapons.filter(weapon => weapon.tier == 1)[0]
+    let shield = itemList.shields.filter(shield => shield.tier == 1)[0]
+
+    return [
+        armor,
+        weapon,
+        shield
+    ];
+}
