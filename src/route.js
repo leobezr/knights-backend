7ca323cfcc801ae9b5ec1c9f3b4f029d7aadd700
leobezr <leonardo.ribeiro@finxi.com.br>
2021-01-17 11:34:57 -0300

    feat: item module

--- src/route.js
+++ src/route.js
@@ -1,19 +1,10 @@
 import CreateEndpoints from "./service/createEndpoints.js";
 import knights from "./service/endpoints/knights.js";
+import items from "./service/endpoints/items.js";
 
 export default function (app) {
    const ENDPOINTS = new CreateEndpoints(app);
 
    ENDPOINTS.set(knights);
-
-   /**
-    * Testing
-    */
-   app.get("/api/v1/teste", async (req, res) => {
-      const users = ["teste", "teste2"];
-
-      res.json({ users });
-   });
-
-   return app;
+   ENDPOINTS.set(items);
 }
