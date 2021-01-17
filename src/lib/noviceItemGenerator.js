dd880499fc1ade9f0d8b4e853ac57524ca1355dd
leobezr <le.bezr@gmail.com>
2021-01-17 15:20:05 -0300

    refactor: changed return

--- src/lib/noviceItemGenerator.js
+++ src/lib/noviceItemGenerator.js
@@ -3,13 +3,15 @@ import deserializeItems from "./deserializeItems";
 export default async function () {
     let itemList = await deserializeItems();
 
-    let armor = itemList.armor.filter(armor => armor.tier == 1)[0]
-    let weapon = itemList.weapons.filter(weapon => weapon.tier == 1)[0]
-    let shield = itemList.shields.filter(shield => shield.tier == 1)[0]
+    let armor = itemList.armor.filter(armor => armor.tier == 1)[0];
+    let weapon = itemList.weapons.filter(weapon => weapon.tier == 1)[0];
+    let shield = itemList.shields.filter(shield => shield.tier == 1)[0];
+    let accessory = itemList.accessories.filter(accessory => accessory.tier == 1)[0];
 
-    return [
+    return {
         armor,
         weapon,
-        shield
-    ];
+        shield,
+        accessory
+    };
 }
