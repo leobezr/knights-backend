6df8a2b63782589fb9a518535d870798cd2f1bfd
leobezr <le.bezr@gmail.com>
2021-01-17 23:08:15 -0300

    feat: item equip

--- src/repositories/UserRepositories.js
+++ src/repositories/UserRepositories.js
@@ -1,18 +1,14 @@
 import fs from "fs";
 import path from "path";
 import KnightFactory from "../lib/knightGenerator.js";
+import KnightEditor from "../lib/knightEditor.js";
 import noviceItemGenerator from "../lib//noviceItemGenerator.js";
 
 export async function getUsers() {
-   try {
-      const DB = fs.readFileSync(path.join(__dirname, "../model/knights.json"));
+   const DB = fs.readFileSync(path.join(__dirname, "../model/knights.json"));
 
-      let data = JSON.parse(DB);
-      return data;
-   } catch (e) {
-      next(e)
-   }
-
+   let data = JSON.parse(DB);
+   return data;
 }
 
 export async function addUser(req) {
@@ -24,17 +20,43 @@ export async function addUser(req) {
       let userData = req.body;
 
       if (!userData) {
-         return false
+         throw "No user data in Body";
       }
 
-      let newKnightConfig = new KnightFactory(userData);
-      newKnightConfig.equip(NOVICE_SET);
+      let knight = new KnightFactory(userData);
+      knight.equip(NOVICE_SET);
 
-      data.push(newKnightConfig);
+      data.push(knight);
 
       fs.writeFileSync(path.join(__dirname, '../model/knights.json'), JSON.stringify(data));
-      return newKnightConfig;
-   } catch (e) {
-      throw new Error(e)
+      return knight;
+   } catch (err) {
+      throw Error(err)
+   }
+}
+
+export async function gearHandler(characterId, equip, removing) {
+   removing = removing || false;
+
+   const CHARACTER_LIST = await getUsers();
+
+   const CHARACTER = CHARACTER_LIST.filter(character => character.id == characterId);
+
+   if (CHARACTER && CHARACTER.length) {
+      let knight = new KnightEditor(CHARACTER[0]);
+
+      if (!removing) {
+         knight.equip(equip);
+      } else {
+         knight.unequip(equip);
+      }
+
+      CHARACTER_LIST.splice(CHARACTER_LIST.indexOf(CHARACTER, 1));
+      CHARACTER_LIST.push(knight.config);
+
+      fs.writeFileSync(path.join(__dirname, '../model/knights.json'), JSON.stringify(CHARACTER_LIST));
+      return knight.config;
+   } else {
+      throw "Character not found";
    }
 }
