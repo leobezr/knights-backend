4d24ec0d0a88a01cc7ebb2d0d2933a0bb35e95b0
leobezr <le.bezr@gmail.com>
2021-01-22 01:28:36 -0300

    fix: heroku error

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const app = express();
-const PORT = process.env.PORT ?? 5000;
+const PORT = process.env.PORT || 5000;
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
