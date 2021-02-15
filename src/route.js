216f26bde0fc8acc3b6ec61542c8ca8d6506d66e
leobezr <le.bezr@gmail.com>
2021-02-14 22:46:11 -0300

    feat: login

--- src/route.js
+++ src/route.js
@@ -2,6 +2,7 @@ import CreateEndpoints from "./service/createEndpoints.js";
 import knights from "./service/endpoints/knights.js";
 import items from "./service/endpoints/items.js";
 import hunt from "./service/endpoints/hunt.js";
+import user from "./service/endpoints/user.js";
 
 export default function (app) {
    const ENDPOINTS = new CreateEndpoints(app);
@@ -9,4 +10,5 @@ export default function (app) {
    ENDPOINTS.set(knights);
    ENDPOINTS.set(items);
    ENDPOINTS.set(hunt);
+   ENDPOINTS.set(user);
 }
